9c60b89656c407f26181f79a3a15489f594c033f
piotrusc <piotrek.usc@gmail.com>
2016-11-09 16:03:03 +0100

    container wrapper implementation

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <cstddef>
+#include <utility>
 
 template<typename T>
-class container_wrapper;
+class container_wrapper{
+public:
+	container_wrapper() = default;
+
+	container_wrapper(T arg): value(std::move(arg)){
+	}
+
+	container_wrapper(const container_wrapper &) = default;
+
+	container_wrapper(container_wrapper &&) = default;
+
+	container_wrapper & operator=(const container_wrapper &) = default;
+
+	container_wrapper & operator=(container_wrapper &&) = default;
+
+	std::size_t size() const{
+		return value.size();
+	}
+private:
+	T value;
+};
 
